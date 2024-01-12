18663c8643f5db91be53618bd431636fd8f54efa
Kareem Mohamed <kareemmohamed@Kareems-Air.lan>
2024-01-12 14:45:37 +0200

    add RTK for fetch apis

---
+++ front-end/src/constants.js
@@ -0,0 +1,7 @@
+export const BASE_URL =
+  process.env.NODE_ENV === "development" ? "http://localhost:5005" : "";
+
+export const PRODUCT_USR = "/api/products";
+export const USERS_USR = "/api/users";
+export const ORDERS_USR = "/api/orders";
+export const PAYPAL_USR = "/api/config/paypal";
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 
+import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 import { router } from "./navigation";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
